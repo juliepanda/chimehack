550fa2d9e490692fda9690dcf0376e89b9c91654
Julie Pan <juliepan@cisco.com>
2015-07-10 16:29:44 -0700

    working simple mail

--- mail.py
+++ mail.py
@@ -1,11 +1,13 @@
 import sendgrid, secrets
 
-sg = sendgrid.SendGridClient(secrets.get_api_username, secrets.get_api_password)
+sg = sendgrid.SendGridClient(secrets.get_api_username(), secrets.get_api_password())
 message = sendgrid.Mail()
 
-message.add_to("test@sendgrid.com")
-message.set_from("you@youremail.com")
-message.set_subject("Sending with SendGrid is Fun")
-message.set_html("and easy to do anywhere, even with Python")
+message.add_to("ycp217@nyu.edu")
+message.set_from("julie.yc.pan@gmail.com")
+message.set_subject("Hi from Chimehack flask app")
+message.set_text("ALOHA THERE <3 <3 <3")
 
-sg.send(message)
+status, msg = sg.send(message)
+
+print status, msg

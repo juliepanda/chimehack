84a64d1de9fbad9b3d6a19c1b78d690db53033f6
Julie Pan <ycp217@nyu.edu>
2015-07-11 12:51:34 -0700

    working email functionality

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
-# import mail.py
+import mail
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        data = request.form['email']
-        return str(data) + ' SUCCESS FROM THE BACKEND'
+        to_email_address = request.form['email']
+        from_email_address = 'julie.yc.pan@gmail.com'
+        subject = 'this is from flask'
+        content = 'i got here yay!'
+        result = mail.send(to_email_address, from_email_address, subject, content)
+        return to_email_address
     if request.method == "GET":
         return render_template('index.html')
 
--- mail.py
+++ mail.py
@@ -1,13 +1,17 @@
 import sendgrid, secrets
 
-sg = sendgrid.SendGridClient(secrets.get_api_username(), secrets.get_api_password())
-message = sendgrid.Mail()
+def test():
+    return 'THIS IS THE SENDGRID MODULE'
 
-message.add_to("ycp217@nyu.edu")
-message.set_from("julie.yc.pan@gmail.com")
-message.set_subject("Hi from Chimehack flask app")
-message.set_text("ALOHA THERE <3 <3 <3")
+def send(to_email_address, from_email_address, subject, content):
+    '''Send email through Sendgrid'''
+    sg = sendgrid.SendGridClient(secrets.get_api_username(), secrets.get_api_password())
+    message = sendgrid.Mail()
+    message.add_to(to_email_address)
+    message.set_from(from_email_address)
+    message.set_subject(subject)
+    message.set_text(content)
 
-status, msg = sg.send(message)
+    status, msg = sg.send(message)
+    return {'status': status, 'msg': msg}
 
-print status, msg

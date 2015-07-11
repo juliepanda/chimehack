d1e6b436a0937c6260f73e60f509236ad8406d2b
Julie Pan <ycp217@nyu.edu>
2015-07-11 14:30:33 -0700

    add mailcontent class for diff template generation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import mail
+import mail, MailContent
 
 app = Flask(__name__)
 
@@ -8,8 +8,8 @@ def index():
     if request.method == "POST":
         to_email_address = request.form['email']
         from_email_address = 'julie.yc.pan@gmail.com'
-        subject = 'this is from flask'
-        content = 'i got here yay!'
+        subject = MailContent.test_obj.subject
+        content = MailContent.test_obj.content
         result = mail.send(to_email_address, from_email_address, subject, content)
         print str(result)
         return to_email_address

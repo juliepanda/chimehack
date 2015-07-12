41522b7b521e2aff36791a12dca8606ba43818ce
Julie Pan <ycp217@nyu.edu>
2015-07-11 17:06:03 -0700

    working texting functionality

--- static/main.js
+++ static/main.js
@@ -3,7 +3,11 @@ window.onload = function() {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
   }
-  function getEmail() {
+  function validatePhoneNumber(number) {
+    var re = /^\d+$/;
+    return re.test(number);
+  }
+  function activateEmailField() {
     var emailBtn = document.getElementById('email-submit-btn');
     emailBtn.addEventListener('click', function() {
       var email = document.getElementById('email-field').value;
@@ -11,7 +15,6 @@ window.onload = function() {
         console.log('this is a valid email address');
         /* now send valid email to backend */
         var jsonObj = { 'email': email };
-        console.log(jsonObj);
         var post = $.post('/sendmail', jsonObj)
         .done( function(data) {
           console.log(JSON.stringify(data));
@@ -24,5 +27,29 @@ window.onload = function() {
       }
     }, false);
   }
-  getEmail();
+  function activateTextField() {
+    var textBtn = document.getElementById('sms-submit-btn');
+    textBtn.addEventListener('click', function () {
+      var n1 = document.getElementById('sms-field-1').value;
+      var n2 = document.getElementById('sms-field-2').value;
+      var n3 = document.getElementById('sms-field-3').value;
+      var number = '1' + n1 + n2 + n3;
+      if(validatePhoneNumber(number)) {
+        /* send valid number to backend */
+        console.log('valid phone number');
+        var jsonObj = { 'number': number };
+        var post = $.post('/sendtext', jsonObj)
+        .done( function(data) {
+          console.log(JSON.stringify(data));
+        })
+        .fail( function(err) {
+          console.log(err);
+        });
+      } else {
+        console.log('not a valid phone number');
+      }
+    });
+  }
+  activateEmailField();
+  activateTextField();
 };
--- text.py
+++ text.py
@@ -7,5 +7,5 @@ def send(from_number, to_number, text):
     '''send SMS via Nexmo service'''
     text = urllib.quote_plus(text)
     r = requests.get('https://rest.nexmo.com/sms/json?' + 'api_key=' + secrets.get_nexmo_key() + '&api_secret=' + secrets.get_nexmo_secret() + '&from=' + from_number + '&to=' + to_number + '&text=' + text)
-    print r.text
+    return r.text
 

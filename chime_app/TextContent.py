d5cb3a2469ea1cc9b56fcaa568f3965e7c2662b1
Tammy Le <letam@sheridancollege.ca>
2015-07-12 13:12:16 -0700

    text content

--- chime_app/TextContent.py
+++ chime_app/TextContent.py
@@ -6,19 +6,19 @@ test_obj = TextContent('Test')
 test_obj.content = 'text test content'
 
 template1 = TextContent('temp1')
-template1.content = 'text test content 1'
+template1.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
 
 template2 = TextContent('temp2')
-template2.content = 'text test content 2'
+template2.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
 
 template3 = TextContent('temp1')
-template3.content = 'text test content 3'
+template3.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
 
 template4 = TextContent('rapeJoke-ouch')
-template4.content = 'mail test content 1'
+template4.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
 
 template5 = TextContent('compSituation-ouch')
-template5.content = 'mail test content 2'
+template5.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
 
 template6 = TextContent('ignoreSign-ouch')
-template6.content = 'mail test content 3'
+template6.content = 'Yesterday, someone noticed that you made a joke about rape. A friend sent this message to give you a heads that it created a hostile environment for women. For more info about why this makes people uncomfortable, or what you can do to help make women safer, visit thinktwiceforchange.com/rapejokes'
